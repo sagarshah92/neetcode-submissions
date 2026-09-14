9ce0de250b7aa7a3c7d4dba49ff4b9df0acebf16
sagarshah92 <sagarkumarshah1992@gmail.com>
2026-09-14 15:47:40 -0500

    Add: triangle - submission-1

---
+++ Data Structures & Algorithms/triangle/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle.size() < 2){
+            return triangle.get(0).get(0);
+        }
+        
+        for (int i = triangle.size()-2; i>=0; i--){
+            List<Integer> currentList = triangle.get(i);
+            for (int j =0; j<currentList.size(); j++){
+                Integer cur = currentList.get(j);
+                int min = cur + Math.min(triangle.get(i+1).get(j),triangle.get(i+1).get(j+1));
+                //System.out.println(min);
+                triangle.get(i).set(j, min);
+            }
+        }
+
+        return triangle.get(0).get(0);
+    }
+}
